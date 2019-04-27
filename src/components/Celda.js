82bfdad44212411d44204e2294b9cdc7183d9620
Santiago Ladavaz <santiagoladavaz@gmail.com>
2019-04-26 23:28:06 -0300

    #1 Creacion de una celda

---
+++ src/components/Celda.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../css/Celda.css';
+
+export class Celda{
+
+    constructor(props){
+        super (props);
+        this.state = {
+            
+        }
+    }
+
+    render () {
+        return (
+        <div className="celda"></div>   
+        )
+    }
+
+}
