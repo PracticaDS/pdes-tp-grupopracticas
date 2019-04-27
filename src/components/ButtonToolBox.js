170acec730670665355ba9a01bd0e64258c38b16
Santiago Ladavaz <santiagoladavaz@gmail.com>
2019-04-27 10:02:23 -0300

    #2 Se crea componente que van dentro del toolbox

---
+++ src/components/ButtonToolBox.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes            from 'prop-types';
+
+export class ButtonToolBox extends Component {
+    render() {
+	    if (this.props.seleccionado){
+          return <div className="ButtonToolbox seleccionado" >
+                    <img src={this.props.src} alt="actions" />
+                 </div>;
+        }else{
+          return <div className="ButtonToolbox" onClick={() => this.props.onSelect(this.props.nombre) } >
+                    <img src={this.props.src} alt="actions"/>
+                 </div>;	
+      }
+  }
+
+
+}
+
+
+ButtonToolBox.propTypes = {
+    nombre: PropTypes.string,
+    seleccionado: PropTypes.bool
+  
+}
