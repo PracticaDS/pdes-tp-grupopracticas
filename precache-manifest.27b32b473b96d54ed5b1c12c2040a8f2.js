41bb21a37b42bf6aaafba7770a15fcb8db87fa7d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-07 23:20:16 +0000

    Deploy PracticaDS/tp-revolucion to github.com/PracticaDS/tp-revolucion.git:gh-pages

---
+++ precache-manifest.27b32b473b96d54ed5b1c12c2040a8f2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b09a66dc3cd431af38316909c451edf",
+    "url": "/tp-revolucion/index.html"
+  },
+  {
+    "revision": "704b82229a167f0753a2",
+    "url": "/tp-revolucion/static/css/main.a155b844.chunk.css"
+  },
+  {
+    "revision": "94d3890432bc45226b2f",
+    "url": "/tp-revolucion/static/js/2.1c70dc0c.chunk.js"
+  },
+  {
+    "revision": "704b82229a167f0753a2",
+    "url": "/tp-revolucion/static/js/main.40242ebd.chunk.js"
+  },
+  {
+    "revision": "ff31b78e643aa7057b25",
+    "url": "/tp-revolucion/static/js/runtime~main.ab84202b.js"
+  },
+  {
+    "revision": "42bc229719c912b352f4c25a714c2055",
+    "url": "/tp-revolucion/static/media/crafter.42bc2297.PNG"
+  },
+  {
+    "revision": "6e7a49f40af2d9d7080f68c833299d78",
+    "url": "/tp-revolucion/static/media/delete.6e7a49f4.PNG"
+  },
+  {
+    "revision": "617ec208f06947f6727cdec4c2c7da85",
+    "url": "/tp-revolucion/static/media/furnace.617ec208.PNG"
+  },
+  {
+    "revision": "cbfcb2fbaf81e234283d1e9e799d3e74",
+    "url": "/tp-revolucion/static/media/move.cbfcb2fb.PNG"
+  },
+  {
+    "revision": "d45efe18b1ff5080c533f30e4fd27ca5",
+    "url": "/tp-revolucion/static/media/rotate.d45efe18.PNG"
+  },
+  {
+    "revision": "3d538c9ed8f3a7cc1026d0de7871c63e",
+    "url": "/tp-revolucion/static/media/seller.3d538c9e.PNG"
+  },
+  {
+    "revision": "8d09d4f3ac899dd528cd58e33fbcb7ff",
+    "url": "/tp-revolucion/static/media/starter.8d09d4f3.PNG"
+  },
+  {
+    "revision": "0c46748fe168eafe811ddfe7e9805bbf",
+    "url": "/tp-revolucion/static/media/transporter.0c46748f.PNG"
+  }
+]);
