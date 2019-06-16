5e5d3ca9ec2eb799bb44ca7482e4334583fe8ec4
Matias <matias.zacarias.e@gmail.com>
2019-06-16 17:36:17 -0300

    se agrega react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Fabrica from './components/Fabrica/Fabrica';
+import Login from './components/Login/Login'
+import Home from './components/Home/Home'
 import store from './store';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute'
 
 
 
@@ -11,12 +15,14 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="Container">
-          <h1>Revolución Industrial</h1>
-          <div className="App"> 
-            <Fabrica />
+        <BrowserRouter>
+          <div className="App">
+            <h1>Revolución Industrial</h1>
           </div>
-        </div>
+          <Route exact path='/' component={Login} />
+          <PrivateRoute path='/home' component={Home} />
+          <PrivateRoute path='/factory' component={Fabrica} />
+        </BrowserRouter>
       </Provider>
     )
   }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import './Login.css'
+
+// import InputGroup from 'react-bootstrap/InputGroup'
+// import FormControl from 'react-bootstrap/FormControl'
+// import Button from 'react-bootstrap/InputGroup'
+
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className="Login container">
+                <p>Login page</p>
+                {/* <br/><br/><br/>
+                <h4 className="text-center">Bienvenido!</h4>
+                <InputGroup className="mb-12 container">
+                    <FormControl
+                    placeholder="usuario"
+                    aria-label="usuario"
+                    aria-describedby="basic-addon2"
+                    />
+                    <InputGroup.Append>
+                    <Button variant="outline-secondary">Ingresar</Button>
+                    </InputGroup.Append>
+                </InputGroup> */}
+            </div>
+        )
+    }
+}
