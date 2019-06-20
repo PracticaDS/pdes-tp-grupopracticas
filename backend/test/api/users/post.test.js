9d2a35f5c96dfff65b84cda441fa7dd2753bb9d0
Matias <matias.zacarias.e@gmail.com>
2019-06-20 16:04:04 -0300

    #21 mejorando api rest de users

--- backend/test/api/users/post.test.js
+++ backend/test/api/users/post.test.js
@@ -2,9 +2,12 @@ process.env.NODE_ENV = 'test'
 
 const expect = require('chai').expect
 const request = require('supertest')
-
 const app = require('../../../src/app')
 const conn = require('../../../db/index')
+const api_url = require('../../../src/config').api_url
+
+
+const api_user = `${api_url}/user`
 
 describe('POST /user', () => {
     before((done) => {
@@ -20,24 +23,24 @@ describe('POST /user', () => {
     })
 
     it('should create a new user', (done) => {
-        request(app).post('/user')
+        request(app).post(api_user)
             .send({username: 'usernamepost', email: 'email@test.com'})
             .then((res) => {
-                const body = res.body
-                expect(body).to.contains.property('_id')
-                expect(body).to.contains.property('username')
-                expect(body).to.contains.property('email')
+                const user = res.body.content
+                expect(user).to.contains.property('_id')
+                expect(user).to.contains.property('username')
+                expect(user).to.contains.property('email')
                 done()
             })
             .catch((err) => done(err))
     })
 
     it('should fail if not send username when create a new user', (done) => {
-        request(app).post('/user')
+        request(app).post(api_user)
             .send({email: 'some text'})
             .then((res) => {
-                const body = res.body
-                expect(body.errors.username.name).to.equal('ValidatorError')
+                const errors = res.body.error.errors
+                expect(errors.username.name).to.equal('ValidatorError')
                 done()
             })
             .catch((err) => done(err))
