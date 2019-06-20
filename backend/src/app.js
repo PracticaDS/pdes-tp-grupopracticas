aa3aabd68f8bb0d3a3367933a37bc00dec60ba48
Matias <matias.zacarias.e@gmail.com>
2019-06-20 16:05:03 -0300

    #22 crud de fabricas

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,11 @@ const bodyParse = require('body-parser')
 const app = express()
 
 const usersRoute = require('./routes/users.route')
+const factoryRoute = require('./routes/factories.route')
 
 app.use(bodyParse.json())
 
 app.use(usersRoute)
+app.use(factoryRoute)
 
 module.exports = app
